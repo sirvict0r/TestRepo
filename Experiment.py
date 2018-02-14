3b9cd70948a571d85f7406b6f070a41af6b2a079
SirVict0r <sirvict0r6@gmail.com>
2018-02-14 00:36:15 -0500

    First commit to address needs!

---
+++ Experiment.py
@@ -0,0 +1,5 @@
+
+class Greeter:
+    def __init__(self):
+        self.message = 'Hello world'
+        # print self.message
---
+++ test_experiment.py
@@ -0,0 +1,8 @@
+import unittest
+from Experiment import Greeter
+
+class MyTestCase(unittest.TestCase):
+    def test_default_greeting_set(self):
+        greeter = Greeter()
+        # this test will fail until you change the Greeter to return this expected message
+        self.assertEqual(greeter.message, 'Hello world!')
