c978d7aea63726ab7dbb6a48db5b76d89038874c
vonWikt0r <sirvict0r6@gmail.com>
2018-03-05 12:38:49 -0500

    test

--- test_experiment.py
+++ test_experiment.py
@@ -3,7 +3,8 @@ from Experiment import Greeter
 
 class MyTestCase(unittest.TestCase):
     def test_default_greeting_set(self):
-        greeter = Greeter()
+        greeter = Mock()
+        greeteror = Greeter(''Hello world!'')
         greeter.return_value = 'Hello world!'
         # this test will fail until you change the Greeter to return this expected message
-        self.assertEqual(greeter, 'Hello world!')
+        self.assertEqual(greeter, greeteror)
